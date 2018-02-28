980283561476f9cb0f8abcf6124aa962f65e6cfb
fcnu7 <tiwari.shaswat7@gmail.com>
2018-02-28 23:11:11 +0545

    Fixed errors

--- header.h
+++ header.h
@@ -9,9 +9,10 @@
 #include "Substitution.c"
 #include "key.c"
 #include "expansion.c"
-#include "permutation.c"
-#include "tobin.c"
-#include "roundfunction.c"
+//#include "permutation.c"
+//#include "tobin.c"
+//#include "roundfunction.c"
+#include "encryption.c"
 
 void expansion(int *,int*);
 void substitution( int [],int []);
@@ -19,13 +20,13 @@ void key_permute(int*,int*);
 void subkey_generation(int* , int a[][48]);
 void subkey_permute(int*,int*);
 void subkey_shift(int*,int*,int[][56]);
-void final_permute(int [64]);
-void init_permute(int [64]);
-void permute_sbox(int [32]);
-void tobin(char [8], int [64]);
+void final_permute(int []);
+void init_permute(int []);
+void permute_sbox(int []);
+void tobin(char [], int []);
 void array_sub(int [],int []);
 void reverse_order(int [],int [],int []);
-void funct(int message1[],message2[],message3[]);
-
+void funct(int [],int [],int []);
+void encryption(int [],int [][48]);
 
 #endif // HEADER_H_INCLUDED
