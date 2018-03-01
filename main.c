7f129f40965c5b2f7ae1ff2ef416f3acfa0b9096
fcnu7 <tiwari.shaswat7@gmail.com>
2018-03-01 23:03:34 +0545

    Fixed major bug and completed back end of project, cheers!

--- main.c
+++ main.c
@@ -4,24 +4,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
-
     // Variable Declarations
     FILE *fptr_plain,        // fptr_plain => input file (plain)
          *fptr_encrypt,      // fptr_encrypt => output file (encrypted)
          *fptr_decrypt;      // fptr_decrypted => decrypted file (plain)
 
-    int bin_message[64], decrypt_msg[8];
+    int bin_message[64];
     int sub_key[16][48];
     int text_count = 0, i, j;
 
     char fchar;
-    char key[] = "HEXKEY12";
+    unsigned char key[] = "HEXKEY12";
 
-    bool en_exit_state = false,
-         de_exit_state = false;
+    bool en_exit_state = false,     //encryption state
+         de_exit_state = false;     //decryption state
 
-    // Open Files
+    // Open Files for encryption
     fptr_plain = fopen("text.txt", "r");
     fptr_encrypt = fopen("encryption.txt", "w");
 
@@ -35,10 +33,10 @@ int main()
     // Generate Sub-keys
     DES_key(key,sub_key);
 
-    // Program Loop
+    // Encryption Loop
     while (! en_exit_state)
     {
-        char txt_message[8] = {'\0'};      //
+        char txt_message[8] = {'\0'};      // array to read from file, initialized to 0
 
         for (text_count = 0; text_count < 8; text_count++)
         {
@@ -53,64 +51,66 @@ int main()
             txt_message[text_count] = fchar;
         }
 
-        tobin(txt_message, bin_message);
-        printf("Before encryption\n");
-        for(i = 0; i < 64; i++){
-            printf("%d",bin_message[i]);
-        }
-        printf("\n");
-        printf("After encryption\n");
-        encryption(bin_message,sub_key);
-        for (i = 0; i < 64;i++)
-        {
-            printf("%d",bin_message[i]);
-        }
-        printf("\n");
+        tobin(txt_message, bin_message);        // convert text to binary
 
+        encryption(bin_message,sub_key);        //encryption function
+
+        // write encrypted message to file
         for(i = 0; i < 64; i++)
         {
             fprintf(fptr_encrypt, "%d", bin_message[i]);
         }
     }
+
+    // close encryption files
     fclose(fptr_plain);
     fclose(fptr_encrypt);
 
+    //Open files for decryption
     fptr_encrypt = fopen("encryption.txt", "r");
     fptr_decrypt = fopen("decryption.txt", "w");
 
-    char o;
+    // File Unavailable Message
+    if (fptr_encrypt == NULL)
+    {
+        printf("File not available!!");
+        exit(1);
+    }
+
+    // variables for decryption
+    int deci_text[8];            //text in ascii value
+    char fchar2;
+
+    // Decryption Loop
     while(! de_exit_state)
     {
-        int dec_message[64] = {0};
+        int decr_message[64] = {0};
 
+        // Get input from file
         for(i = 0; i < 64; i++)
         {
-            o = getc(fptr_encrypt);
+            fchar2 = getc(fptr_encrypt);
 
-            if (o == EOF)
+            if (fchar2 == EOF)
             {
                 de_exit_state = true;
                 break;
             }
 
-            dec_message[i] = atoi(&o);
-            printf("%d", dec_message[i]);
+            decr_message[i] = atoi(&fchar2);
         }
 
-        //printf("After decryption\n");
-        decryption(dec_message,sub_key);
-        todeci(dec_message, decrypt_msg);
+        decryption(decr_message,sub_key);       // Decryption Function
+        todeci(decr_message, deci_text);        // binary to decimal(ascii)
 
+        // Write decrypted message to file
         for (j = 0; j < 8; j++)
         {
-            fprintf(fptr_decrypt, "%c", decrypt_msg[j]);
+            fprintf(fptr_decrypt, "%c", deci_text[j]);
         }
-        /*for(i = 0; i < 64; i++)
-        {
-            printf("%d",dec_message[i]);
-        }*/
     }
 
+    // Close Decryption Files
     fclose(fptr_encrypt);
     fclose(fptr_decrypt);
 
